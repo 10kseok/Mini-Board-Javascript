1be6e4cbb0d4d99c9ad8c2ea972a37f97586e5cd
koesnam <76582376+10kseok@users.noreply.github.com>
2024-04-16 13:54:07 +0900

    chore: (Front) 작성자 태그 해싱 로직 변경
    기존 해싱은 충돌이 잦아 색깔이 겹쳐서, 충돌이 덜 일어나는 로직으로 변경

--- frontend/src/components/views/BoardPage/PostDetailPage.js
+++ frontend/src/components/views/BoardPage/PostDetailPage.js
@@ -1,7 +1,6 @@
 import { Typography } from "antd";
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
 import CommentSection from "./comment/CommentSection";
 
--- frontend/src/components/views/LandingPage/LandingPage.js
+++ frontend/src/components/views/LandingPage/LandingPage.js
@@ -41,7 +41,7 @@ function LandingPage() {
     function hashCode(str) {
         let hash = 0;
         for (let i = 0; i < str.length; i++) {
-           hash = str.charCodeAt(i) + ((hash << 5) - hash);
+           hash += str.charCodeAt(i);
         }
         return hash;
     } 
